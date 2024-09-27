91af377c0f525d3d5261081aa20cf76ed9ae450d
oxanadev <shana_bj@hotmail.com>
2024-09-27 20:24:22 +0500

    Added table

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+// import Header.css from "./header"
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h1>English cards</h1>
+      <button></button>
+      <button></button>
+    </div>
+  );
+}
+
+export default Header;
