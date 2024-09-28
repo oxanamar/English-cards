8b85ab0cc670f969547cddebb9b108260d697385
oxanadev <shana_bj@hotmail.com>
2024-09-28 21:18:22 +0500

    Added table

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../Header/Header";
 import Table from "../Table/Table";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Table />
+      <Footer />
     </div>
   );
 }
